36a48c345aa5196c6df5644a9ccc41d96c3beb7c
iykaea <iykaea5@gmail.com>
2026-05-06 03:19:33 +0100

    cleanup: Removing scripts not required for assignment

--- scripts/userinfo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p ../logs
-echo "starting user registration"
-read -p "Enter your Name: " NAME 
-read -p "Enter your Age: " AGE 
-read -p "Enter your Country: " COUNTRY
-echo "Hello $NAME from $COUNTY!"
